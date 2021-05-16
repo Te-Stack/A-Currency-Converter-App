8bfa96333d6f04459ac4bbfc14f6c0023ecb0ba4
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-05-16 22:02:42 +0100

    updated the current value

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ const inserted = document.getElementById("myinput");
 inserted.addEventListener('input',function(event){
     let converted = event.target.value
     let dollarValue = document.getElementById("dollarsid");
-    dollarValue.innerHTML = converted * 365
+    dollarValue.innerHTML = converted * 413
     let poundValue = document.getElementById("poundsid");
-    poundValue.innerHTML = converted * 420
+    poundValue.innerHTML = converted * 582.48
     let euroValue = document.getElementById("eurosid");
-    euroValue.innerHTML = converted *440
+    euroValue.innerHTML = converted *501.63
     let cedisValue = document.getElementById("cedisid");
-    cedisValue.innerHTML = converted *50
+    cedisValue.innerHTML = converted *71.65
 })
 
