f073b5739fea9bf372631e81492f743a8d860fd5
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-06-08 00:11:48 +0100

    currency update

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ const inserted = document.getElementById("myinput");
 inserted.addEventListener('input',function(e){
     let converted = e.target.value
     let dollarValue = document.getElementById("dollarsid");
-    dollarValue.innerHTML = converted * 412.69
+    dollarValue.innerHTML = converted * 412.54
     let poundValue = document.getElementById("poundsid");
-    poundValue.innerHTML = converted * 585.34
+    poundValue.innerHTML = converted * 584.89
     let euroValue = document.getElementById("eurosid");
-    euroValue.innerHTML = converted *502.94
+    euroValue.innerHTML = converted *502.97
     let cedisValue = document.getElementById("cedisid");
     cedisValue.innerHTML = converted *71.02
 })
